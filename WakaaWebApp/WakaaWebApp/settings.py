eaed6b4e6dd9d245f35e14dc030894b8bb301922
Akim <jakim2621@gmail.com>
2023-11-22 12:54:18 +0100

    creation de l'application wakaa

--- WakaaWebApp/WakaaWebApp/settings.py
+++ WakaaWebApp/WakaaWebApp/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'WakaaWebApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wakaawebapp',
+        'USER': 'Root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': [
+            
+        ]
     }
 }
 
